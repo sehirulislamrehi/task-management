093c12285dfb828ab7aed349d07f0aa519b2ade7
Sehirul Islam Rehi <mdsehirulislamrehi@gmail.com>
2024-10-19 17:23:25 +0600

    .

--- app/Traits/FilePathTrait.php
+++ app/Traits/FilePathTrait.php
@@ -3,13 +3,7 @@
 namespace App\Traits;
 
 
-/**
- * Conatain some utility function to handel the response for api
- * @author Alimul-Mahfuz <alimulmahfuztushar@gamil.com>
- * @method mixed success() Handle success response
- * @method mixed error() Handle error response
- * @copyright 2023 MIS PRAN-RFL Group
- */
+
 trait FilePathTrait
 {
 
